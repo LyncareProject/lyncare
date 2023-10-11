bf9db98e467bc25fa82216c6367d386776d13c1d
Kun Woo Kim <kimkuns98@gmail.com>
2023-10-11 16:32:48 +0900

    Setting Test

--- server/src/app.js
+++ server/src/app.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(path.join(__dirname, '../../client/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 app.use((err, req, res, next) => {
@@ -22,7 +22,7 @@ app.use((err, req, res, next) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 module.exports = app
