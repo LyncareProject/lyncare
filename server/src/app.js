0397ddc4c7eef8f2ed358550abd3dbe3a6902b57
Kun Woo Kim <kimkuns98@gmail.com>
2023-10-25 11:34:26 +0900

    add https

--- server/src/app.js
+++ server/src/app.js
@@ -1,32 +1,42 @@
-// @ts-check
-
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 
-const app = express()
+const app = express();
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    const httpsHost = req.headers.host.replace(/:\d+$/, "");
+    console.log(httpsHost);
+    const httpsUrl = `https://${httpsHost}${req.url}`;
+    console.log(httpsUrl);
+    res.redirect(301, httpsUrl);
+  }
+});
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const mainRouter = require('./routes/index')
+const mainRouter = require("./routes/index");
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
-app.use('/api', mainRouter)
+app.use("/api", mainRouter);
 
 app.use((err, req, res, next) => {
-  res.statusCode = err.statusCode || 500
-  res.send(err.message)
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.statusCode = err.statusCode || 500;
+  res.send(err.message);
 });
 
-module.exports = app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = app;
